041dbd89052f010127f22812b6c3366733fdb01d
daniballe <danielaballe1901@gmail.com>
2025-11-22 09:53:56 -0500

    Update api.jsx

--- src/api.jsx
+++ src/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = import.meta.env.VITE_API_BASE || 'https://daniballe19.github.io/game-traker-back/';
+const API_BASE = import.meta.env.VITE_API_BASE || 'https://github.com/daniballe19/game-traker-front.git';
 
 export const api = axios.create({
   baseURL: API_BASE
